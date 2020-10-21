6a8ecd0f7fb098bb5ca631bfbc0f013676b1f3cf
Tyler Hasenoehrl <tylrhas@gmail.com>
2020-10-21 16:29:46 -0700

    get it working for service account

--- server/routes/api/link-discoverer.js
+++ server/routes/api/link-discoverer.js
@@ -1,14 +1,37 @@
 const discoverLinks = require('../../controllers/google-auth')
 
 module.exports = (app) => {
-  app.post('/link-dicoverer', async (req, res) => {
-    try {
-      const { body } = req
-      const response = await discoverLinks(body)
-      res.status(200).json(response)
-    } catch (err) {
-      console.error(err)
-      res.status(500).send(err)
+  app.post('/link-dicoverer', (req, res) => {
+    const request = require('request-promise')
+    const { LINK_DISCOVERER_URL: receivingServiceURL, DISCOVER_URL } = process.env
+    console.log({ receivingServiceURL, DISCOVER_URL })
+
+    // Set up metadata server request
+    // See https://cloud.google.com/compute/docs/instances/verifying-instance-identity#request_signature
+    const metadataServerTokenURL = 'http://metadata/computeMetadata/v1/instance/service-accounts/default/identity?audience='
+    const tokenRequestOptions = {
+      uri: metadataServerTokenURL + receivingServiceURL,
+      headers: {
+        'Metadata-Flavor': 'Google'
+      }
     }
+    // Fetch the token, then provide the token in the request to the receiving service
+    request(tokenRequestOptions)
+      .then((token) => {
+        return request({
+          method: 'POST',
+          uri: DISCOVER_URL,
+          body: {
+            url: 'http://all-americanselfstorage.com/'
+          },
+          json: true
+        }).auth(null, null, true, token)
+      })
+      .then((response) => {
+        res.status(200).send(response)
+      })
+      .catch((error) => {
+        res.status(400).send(error)
+      })
   })
 }
