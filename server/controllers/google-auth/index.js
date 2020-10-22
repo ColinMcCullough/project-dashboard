a4fabdcb6dec9d03a4e5b73e9630fee85762b390
David Miller <david.miller@getg5.com>
2020-10-22 09:49:28 -0700

    back to nonsense

--- server/controllers/google-auth/index.js
+++ server/controllers/google-auth/index.js
@@ -1,36 +1,23 @@
-
-const { GoogleAuth } = require('google-auth-library')
 const axios = require('axios')
-const auth = new GoogleAuth()
-
-let client, serviceUrl
 
 module.exports = async (body) => {
   if (!process.env.LINK_DISCOVERER_URL) throw Error('Service URL variable needs to be set.')
-  serviceUrl = `${process.env.LINK_DISCOVERER_URL}/discover`
-  const serviceRequestOptions = {
-    method: 'POST',
-    headers: {
-      'Content-Type': 'application/json'
-    },
-    body,
-    timeout: 3000
-  }
 
-  try {
-    if (!client) client = await auth.getIdTokenClient(serviceUrl)
-    const clientHeaders = await client.getRequestHeaders()
-    serviceRequestOptions.headers.Authorization = clientHeaders['Authorization']
-  } catch(err) {
-    throw Error('could not create an identity token: ', err)
+  const { LINK_DISCOVERER_URL } = process.env
+  const metadataServerTokenURL = 'http://metadata/computeMetadata/v1/instance/service-accounts/default/identity?audience='
+  const tokenRequestOptions = {
+    uri: metadataServerTokenURL + LINK_DISCOVERER_URL,
+    headers: { 'Metadata-Flavor': 'Google' }
   }
-
-  try {
-    console.log('pre-request', { serviceUrl, serviceRequestOptions })
-    const serviceResponse = await axios(serviceUrl, serviceRequestOptions)
-    console.log('post-request', { serviceResponse })
-    return serviceResponse.body
-  } catch (err) {
-    throw Error('Failed: ', err)
-  }
+  return await axios(tokenRequestOptions)
+    .then(async (token) => {
+      return await axios({
+        method: 'POST',
+        uri: `${LINK_DISCOVERER_URL}/discover`,
+        body: { url },
+        headers: {
+          'Authorization': `Bearer ${token}`
+        }
+      })
+    })
 }
--- server/routes/api/link-discoverer.js
+++ server/routes/api/link-discoverer.js
@@ -1,29 +1,27 @@
-const discoverLinks = require('../../controllers/google-auth')
-const axios = require('axios')
-
 module.exports = (app) => {
-  app.post('/api/v1/discover', (req, res) => {
-    const { url } = req.body
-    const { LINK_DISCOVERER_URL } = process.env
+  app.post('/link-dicoverer', (req, res) => {
+    // const { url } = req.body
+    const request = require('request-promise')
+    const { LINK_DISCOVERER_URL: receivingServiceURL, DISCOVER_URL } = process.env
     const metadataServerTokenURL = 'http://metadata/computeMetadata/v1/instance/service-accounts/default/identity?audience='
     const tokenRequestOptions = {
-      uri: metadataServerTokenURL + LINK_DISCOVERER_URL,
+      uri: metadataServerTokenURL + receivingServiceURL,
       headers: { 'Metadata-Flavor': 'Google' }
     }
-    axios(tokenRequestOptions)
+
+    request(tokenRequestOptions)
       .then((token) => {
-        return axios({
+        return request({
           method: 'POST',
-          uri: `${LINK_DISCOVERER_URL}/discover`,
-          body: { url },
+          uri: DISCOVER_URL,
+          body: {
+            url: 'http://all-americanselfstorage.com/'
+            // url
+          },
           json: true
         }).auth(null, null, true, token)
       })
-      .then((response) => {
-        res.status(200).send(response)
-      })
-      .catch((error) => {
-        res.status(400).send(error)
-      })
+      .then(response => res.status(200).send(response))
+      .catch(error => res.status(400).send(error))
   })
 }
