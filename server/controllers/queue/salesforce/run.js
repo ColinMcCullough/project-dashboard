1d65a5da292105c1ff90248219a33f4ee18f11de
Tyler Hasenoehrl <tylrhas@gmail.com>
2020-10-29 09:36:33 -0700

    add grpc

--- server/controllers/queue/salesforce/run.js
+++ server/controllers/queue/salesforce/run.js
@@ -5,4 +5,39 @@ module.exports = {
 
 async function processor(job) {
 
+  // UPDATE THE PATH
+  const PROTO_PATH = __dirname + '/proto/linkDiscoverer.proto';
+
+  const grpc = require('@grpc/grpc-js');
+  const protoLoader = require('@grpc/proto-loader');
+  
+  let packageDefinition = protoLoader.loadSync(
+      PROTO_PATH,
+      {keepCase: true,
+       longs: String,
+       enums: String,
+       defaults: true,
+       oneofs: true
+      });
+  let employee_proto = grpc.loadPackageDefinition(packageDefinition).linkDiscoverer;
+  
+  function main() {
+    let client = new employee_proto.LinkDiscoverer('localhost:4500',
+                                         grpc.credentials.createInsecure());
+                                         
+    // let employeeIdList = [1,10,2];
+    // let call = client.paySalary({employeeIdList: employeeIdList});
+    let call = client.discoverLinks({ url: 'https://solaire8250.com/' })
+  
+    call.on('data',function(response){
+      console.log({ response });
+    });
+  
+    call.on('end',function(){
+      console.log('Done');
+    });
+  
+  }
+  
+  main();
 }
