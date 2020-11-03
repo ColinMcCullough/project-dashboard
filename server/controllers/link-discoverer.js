87ab5883d63cd6464b8c0d89d08c0f78fb8d65d3
Tyler Hasenoehrl <tylrhas@gmail.com>
2020-11-03 14:45:39 -0800

    add pubsub

--- server/controllers/link-discoverer.js
+++ server/controllers/link-discoverer.js
@@ -10,22 +10,22 @@ module.exports = linkDiscoverer
  * @returns {Array} data
  * @throws {Error} Invalid arguement error / invalid response
  */
-async function linkDiscoverer(url) {
+async function linkDiscoverer(url, topicName) {
   if (!linkDiscovererUrl) {
-    throw Error('Invalid arguement passed in')
+    throw new Error('Invalid arguement passed in')
   }
   try {
     const devToken = process.env.TOKEN
-    const bearerToken = devToken ? devToken : await getBearerToken(linkDiscovererUrl)
+    const bearerToken = devToken || await getBearerToken(linkDiscovererUrl)
     const postUrl = linkDiscovererUrl
-    const body = { url }
+    const body = { url, topicName }
     const config = {
       headers: { Authorization: `Bearer ${bearerToken}` }
     }
     const res = await axios.post(postUrl, body, config)
-    if (res.status !== 200) throw Error(`Link discoverer Failed: Status ${res.status}`)
+    if (res.status !== 200) { throw new Error(`Link discoverer Failed: Status ${res.status}`) }
     return res.data
-  } catch(err) {
+  } catch (err) {
     return err
   }
 }
--- server/controllers/queue/link-discoverer/run.js
+++ server/controllers/queue/link-discoverer/run.js
@@ -1,16 +1,19 @@
 const linkDiscoverer = require('../../link-discoverer')
-const { createAndSubscribe } = require('../../pubsub');
-
+const { createAndSubscribe } = require('../../pubsub')
+const { GCP_PROJECT_ID } = process.env
 module.exports = {
   processor,
   concurrency: 1
 }
 
 async function processor(job, done) {
-  const subscription = await createAndSubscribe()
+  const { id } = job
+  const topicName = `linkDiscover_${id}`
+  const subscriptionName = `${topicName}_projectDashboard`
+  const subscription = await createAndSubscribe(GCP_PROJECT_ID, topicName, subscriptionName)
   subscription.on('message', (message) => {
     console.log('Received message:', message.data)
     message.ack()
   })
-  await linkDiscoverer(job.data.url)
+  await linkDiscoverer(job.data.url, topicName)
 }
