3ec53a40f4ce6cd7f34e395fdd61c5c27c443ea7
Tyler Hasenoehrl <tylrhas@gmail.com>
2020-11-04 09:22:36 -0800

    remove auth

--- server/controllers/link-discoverer.js
+++ server/controllers/link-discoverer.js
@@ -16,13 +16,13 @@ async function linkDiscoverer(url, topicName) {
   }
   try {
     const devToken = process.env.TOKEN
-    const bearerToken = devToken || await getBearerToken(linkDiscovererUrl)
+    // const bearerToken = devToken || await getBearerToken(linkDiscovererUrl)
     const postUrl = linkDiscovererUrl
     const body = { url, topicName }
     const config = {
       headers: { Authorization: `Bearer ${bearerToken}` }
     }
-    const res = await axios.post(postUrl, body, config)
+    const res = await axios.post(postUrl, body)
     if (res.status !== 200) { throw new Error(`Link discoverer Failed: Status ${res.status}`) }
     return res.data
   } catch (err) {
